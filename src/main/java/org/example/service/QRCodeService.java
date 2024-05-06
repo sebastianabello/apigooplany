e8d5a66151b9a7f8251e2bbbbacf435e30813429
Sebastian Abello <juanse.abello.2004@gmail.com>
2024-05-06 12:25:08 -0500

    QrGenerated-test

---
+++ src/main/java/org/example/service/QRCodeService.java
@@ -0,0 +1,20 @@
+package org.example.service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Service;
+
+import java.awt.image.BufferedImage;
+
+@Service
+public class QRCodeService {
+    public BufferedImage generateQRCode(String text, int width, int height) throws Exception {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+        MatrixToImageConfig config = new MatrixToImageConfig(0xFF000000, 0xFFF5DEB3);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix,config);
+    }
+}
