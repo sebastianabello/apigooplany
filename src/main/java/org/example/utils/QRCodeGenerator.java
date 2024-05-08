27ac4c77f8067d18bdb9f7e6536741c074d9a89d
Sebastian Abello <juanse.abello.2004@gmail.com>
2024-05-07 22:48:55 -0500

    Corregido error de QRCOdeGenerador

--- src/main/java/org/example/utils/QRCodeGenerator.java
+++ src/main/java/org/example/utils/QRCodeGenerator.java
@@ -5,7 +5,6 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import org.example.persistence.entity.UserEntity;
 import org.example.presentation.dto.UserDTO;
 
 import java.io.IOException;
